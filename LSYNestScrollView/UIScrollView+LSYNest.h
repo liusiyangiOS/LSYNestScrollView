90e46d0743a60d4d0cf0df9a6c3dae4e6752a713
liusiyang05 <liusiyang05@58.com>
2025-02-08 19:02:00 +0800

    开发sdk

--- LSYNestScrollView/UIScrollView+LSYNest.h
+++ LSYNestScrollView/UIScrollView+LSYNest.h
@@ -11,14 +11,42 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIScrollView (LSYNest)
 
+//@property (nonatomic, assign) BOOL lsyNest_shouldScroll;
+
+/**
+ 将ScrollView注册为嵌套模式的主ScrollView(最外层的)
+ @param delegate ScrollView的delegate
+ @param key 嵌套ScrollView体系的key,会将相同的key的ScrollView进行关联
+ */
 - (void)lsyNest_registerAsMainWithDelegate:(id<UIScrollViewDelegate>)delegate forKey:(NSString *)key;
 
 /**
- 自动索引+1
+ 将ScrollView注册为嵌套模式的子ScrollView(内部的),根据添加顺序自动设置index
+ @param delegate ScrollView的delegate
+ @param key 嵌套ScrollView体系的key,会将相同的key的ScrollView进行关联
  */
 - (void)lsyNest_registerAsInnerWithDelegate:(id<UIScrollViewDelegate>)delegate forKey:(NSString *)key;
 
+/**
+ 将ScrollView注册为嵌套模式的子ScrollView(内部的)
+ @param delegate ScrollView的delegate
+ @param index 指定ScrollView所在位置的索引,遇到复杂业务逻辑的时候可能会用的上,若小于0,则根据添加顺序自动设置index
+ @param key 嵌套ScrollView体系的key,会将相同的key的ScrollView进行关联
+ */
 - (void)lsyNest_registerAsInnerWithDelegate:(id<UIScrollViewDelegate>)delegate ofIndex:(NSInteger)index forKey:(NSString *)key;
+
+/** 将自己设置为active */
+- (void)lsyNest_setActive;
+
+/**
+ 将index对应的ScrollView设置为active,即与main联动的状态(默认与第一个注册的联动)
+ */
++ (void)lsyNest_setActiveIndex:(NSInteger)index forKey:(NSString *)key;
+
+/**
+ 删除key对应的嵌套结构
+ */
++ (void)lsyNest_removeStructureForKey:(NSString *)key;
 
 @end
 
--- LSYNestScrollView/UIScrollView+LSYNest.m
+++ LSYNestScrollView/UIScrollView+LSYNest.m
@@ -6,19 +6,119 @@
 //
 
 #import "UIScrollView+LSYNest.h"
+#import <objc/runtime.h>
+
+@interface LSYScrollViewNestStructure : NSObject
+
+@property (nonatomic, weak) UIScrollView *mainScrollView;
+
+@property (nonatomic, strong, readonly) NSMapTable *innerScrollViews;
+
+@end
+
+@implementation LSYScrollViewNestStructure
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _innerScrollViews = [NSMapTable mapTableWithKeyOptions:NSPointerFunctionsStrongMemory valueOptions:NSPointerFunctionsWeakMemory];
+    }
+    return self;
+}
+
+@end
+
+@interface LSYScrollViewNestParam : NSObject
+
+@property (nonatomic, assign) BOOL isMainScrollView;
+/** 联动的ScrollView,只有mainScrollView才有值 */
+@property (nonatomic, weak) UIScrollView *activeScrollView;
+/** 主ScrollView,只有innerScrollView才有值 */
+@property (nonatomic, weak) UIScrollView *mainScrollView;
+
+@property (nonatomic, assign) BOOL shouldScroll;
+
+@end
+
+@implementation LSYScrollViewNestParam
+
+@end
 
 @implementation UIScrollView (LSYNest)
 
 - (void)lsyNest_registerAsMainWithDelegate:(id<UIScrollViewDelegate>)delegate forKey:(NSString *)key{
-    
+    LSYScrollViewNestStructure *structure = [[UIScrollView lsyNest_structureMap] objectForKey:key];
+    structure.mainScrollView = self;
+    LSYScrollViewNestParam *param = [self lsyNest_param];
+    param.isMainScrollView = YES;
+    if (structure.innerScrollViews.count) {
+        //默认第一个注册的inner与main联动
+        [structure.mainScrollView lsyNest_param].activeScrollView = self;
+    }
 }
 
 - (void)lsyNest_registerAsInnerWithDelegate:(id<UIScrollViewDelegate>)delegate forKey:(NSString *)key{
-    
+    [self lsyNest_registerAsInnerWithDelegate:delegate ofIndex:-1 forKey:key];
 }
 
 - (void)lsyNest_registerAsInnerWithDelegate:(id<UIScrollViewDelegate>)delegate ofIndex:(NSInteger)index forKey:(NSString *)key{
-    
+    LSYScrollViewNestStructure *structure = [[UIScrollView lsyNest_structureMap] objectForKey:key];
+    if (structure.mainScrollView && !structure.innerScrollViews.count) {
+        //默认第一个注册的inner与main联动
+        [structure.mainScrollView lsyNest_param].activeScrollView = self;
+    }
+    if (index < 0) {
+        index = structure.innerScrollViews.count;
+    }
+    [structure.innerScrollViews setObject:self forKey:@(index)];
+    LSYScrollViewNestParam *param = [self lsyNest_param];
+    param.mainScrollView = structure.mainScrollView;
 }
+
+- (void)lsyNest_setActive{
+    UIScrollView *mainScrollView = [self lsyNest_param].mainScrollView;
+    [mainScrollView lsyNest_param].activeScrollView = self;
+}
+
++ (void)lsyNest_setActiveIndex:(NSInteger)index forKey:(NSString *)key{
+    LSYScrollViewNestStructure *structure = [[self lsyNest_structureMap] objectForKey:key];
+    UIScrollView *scrollView = [structure.innerScrollViews objectForKey:@(index)];
+    [scrollView lsyNest_setActive];
+}
+
++ (void)lsyNest_removeStructureForKey:(NSString *)key{
+    [[self lsyNest_structureMap] removeObjectForKey:key];
+}
+
+#pragma mark - private method
+
+#pragma mark - setter & getter
+
++ (NSMutableDictionary *)lsyNest_structureMap{
+    static NSMutableDictionary *lsyNest_structureMap;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        lsyNest_structureMap = [NSMutableDictionary dictionary];
+    });
+    return lsyNest_structureMap;
+}
+
+-(LSYScrollViewNestParam *)lsyNest_param{
+    LSYScrollViewNestParam *param = objc_getAssociatedObject(self, _cmd);
+    if (!param) {
+        param = [LSYScrollViewNestParam new];
+        objc_setAssociatedObject(self, _cmd, param, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return param;
+}
+
+//- (void)setLsyNest_shouldScroll:(BOOL)lsyNest_shouldScroll{
+//    
+//}
+//
+//-(BOOL)lsyNest_shouldScroll{
+//    
+//}
 
 @end
